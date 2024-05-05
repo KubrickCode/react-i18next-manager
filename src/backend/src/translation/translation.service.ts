6a1775af6b68cd84a5c0c770226459870f95e1ab
KubrickCode <kubrickcode@gmail.com>
2024-05-05 05:18:52 +0000

    백엔드 서비스에서 기존 json 파일 데이터 형식 변환해서 반환하도록 수정(임시 하드코딩)

--- src/backend/src/translation/translation.service.ts
+++ src/backend/src/translation/translation.service.ts
@@ -6,6 +6,31 @@ export class TranslationService {
   constructor(private readonly translationRepository: TranslationRepository) {}
 
   async getTranslations() {
-    return await this.translationRepository.getTranslations();
+    const translations = await this.translationRepository.getTranslations();
+    const i18n = {
+      keys: {
+        common: {
+          en: {
+            create: translations.en["ui.common.create"],
+            delete: translations.en["ui.common.delete"],
+          },
+          ko: {
+            create: translations.ko["ui.common.create"],
+            delete: translations.ko["ui.common.delete"],
+          },
+        },
+        error: {
+          en: {
+            createError: translations.en["ui.error.createError"],
+            deleteError: translations.en["ui.error.deleteError"],
+          },
+          ko: {
+            createError: translations.ko["ui.error.createError"],
+            deleteError: translations.ko["ui.error.deleteError"],
+          },
+        },
+      },
+    };
+    return i18n;
   }
 }
