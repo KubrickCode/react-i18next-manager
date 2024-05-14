d453bc62fb36de02f2b4a06229c2a89d9f3071b2
KubrickCode <kubrickcode@gmail.com>
2024-05-14 12:19:29 +0000

    트리뷰 스타일 코드 수정
    - colormode에 대응할 수 있도록 수정
    - input size 수정
    - box radius 설정
    - box gap 설정
    - box padding 설정

--- src/frontend/src/layout/sidebar/layout-sidebar-group-treeview.tsx
+++ src/frontend/src/layout/sidebar/layout-sidebar-group-treeview.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Box, Flex, VStack } from "@chakra-ui/react";
+import { Box, Flex, VStack, useColorModeValue } from "@chakra-ui/react";
 import { SearchInput } from "@saas-ui/react";
 import { FaEdit, FaPlus, FaSave, FaTrash } from "react-icons/fa";
 import { MdArrowDropDown, MdArrowRight } from "react-icons/md";
@@ -58,77 +58,98 @@ const Node = ({ node, tree }: NodeRendererProps<TreeData>) => {
   const [input, setInput] = useState(node.data.name);
   const [isHovered, setIsHovered] = useState(false);
 
+  const treeNodeBgColor = useColorModeValue("gray.100", "gray.700");
+  const inputBgColor = useColorModeValue("white", "gray.800");
+
   return (
     <Box
-      _hover={{ backgroundColor: "gray.700" }}
+      _hover={{ backgroundColor: treeNodeBgColor }}
       alignItems="center"
-      backgroundColor={node.state.isSelected ? "gray.700" : "transparent"}
+      backgroundColor={node.state.isSelected ? treeNodeBgColor : "transparent"}
+      borderRadius={5}
       cursor="pointer"
       display="flex"
+      gap={2}
       height={45}
       onClick={() => node.isInternal && node.toggle()}
       onMouseOver={() => setIsHovered(true)}
       onMouseOut={() => setIsHovered(false)}
-      paddingLeft={`${(node.level + 1) * 2}rem`}
+      paddingLeft={`${(node.level + 1) * 1.5}rem`}
+      paddingRight="0.5rem"
       width="full"
     >
-      {node.isEditing ? (
-        <Input onChange={(e) => setInput(e.target.value)} value={input} />
-      ) : (
-        <Flex alignContent="flex-start" alignItems="center" width="full">
-          <Text>{node.data.name}</Text>
-          {node.data.children &&
-            (node.isOpen ? <MdArrowDropDown /> : <MdArrowRight />)}
-        </Flex>
-      )}
-      {(node.isSelected || isHovered) &&
-        (node.isEditing ? (
-          <IconButton
-            aria-label="save-edit"
-            icon={<FaSave />}
-            onClick={(e) => {
-              e.stopPropagation();
-              node.submit(input);
-            }}
-            size="xs"
-            variant="ghost"
+      <Flex alignContent="flex-start" alignItems="center" width="full">
+        {node.isEditing ? (
+          <Input
+            autoFocus
+            backgroundColor={inputBgColor}
+            onChange={(e) => setInput(e.target.value)}
+            size="sm"
+            value={input}
           />
         ) : (
+          <>
+            <Text>{node.data.name}</Text>
+            {node.data.children &&
+              (node.isOpen ? <MdArrowDropDown /> : <MdArrowRight />)}
+          </>
+        )}
+      </Flex>
+
+      <Flex>
+        {(node.isSelected || isHovered) &&
+          (node.isEditing ? (
+            <IconButton
+              aria-label="save-edit"
+              colorScheme="gray"
+              icon={<FaSave />}
+              onClick={(e) => {
+                e.stopPropagation();
+                node.submit(input);
+              }}
+              size="xs"
+              variant="ghost"
+            />
+          ) : (
+            <IconButton
+              aria-label="edit"
+              colorScheme="gray"
+              icon={<FaEdit />}
+              onClick={(e) => {
+                e.stopPropagation();
+                node.edit();
+              }}
+              size="xs"
+              variant="ghost"
+            />
+          ))}
+        {node.isSelected && (
           <IconButton
-            aria-label="edit"
-            icon={<FaEdit />}
+            aria-label="add"
+            colorScheme="gray"
+            icon={<FaPlus />}
             onClick={(e) => {
               e.stopPropagation();
-              node.edit();
+              tree.createInternal();
             }}
             size="xs"
             variant="ghost"
           />
-        ))}
-      {node.isSelected && (
-        <IconButton
-          aria-label="add"
-          icon={<FaPlus />}
-          onClick={(e) => {
-            e.stopPropagation();
-            tree.createInternal();
-          }}
-          size="xs"
-          variant="ghost"
-        />
-      )}
-      {(node.isSelected || isHovered) && (
-        <IconButton
-          aria-label="delete"
-          icon={<FaTrash />}
-          onClick={(e) => {
-            e.stopPropagation();
-            tree.delete(node.id);
-          }}
-          size="xs"
-          variant="ghost"
-        />
-      )}
+        )}
+        {(node.isSelected || isHovered) && (
+          <IconButton
+            aria-label="delete"
+            colorScheme="gray"
+            icon={<FaTrash />}
+            onClick={(e) => {
+              e.stopPropagation();
+              tree.delete(node.id);
+            }}
+            size="xs"
+            variant="ghost"
+          />
+        )}
+      </Flex>
     </Box>
   );
 };
