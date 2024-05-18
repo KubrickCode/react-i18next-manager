d626f26abfa9ca10a9ef99f7e53e751e81938e8c
KubrickCode <kubrickcode@gmail.com>
2024-05-18 13:54:15 +0000

    fix: SearchInput 누락된 value 설정

--- src/frontend/src/layout/sidebar/layout-sidebar-group-treeview.tsx
+++ src/frontend/src/layout/sidebar/layout-sidebar-group-treeview.tsx
@@ -32,6 +32,7 @@ export const LayoutSidebarGroupTreeView = () => {
         onChange={(e) => setTerm(e.target.value)}
         onReset={() => setTerm("")}
         size="sm"
+        value={term}
       />
       <Box
         _hover={{ backgroundColor: treeNodeBgColor }}
