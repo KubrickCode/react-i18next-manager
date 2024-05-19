344aa219b0254403b514c03aa6d46780f134f7a3
KubrickCode <kubrickcode@gmail.com>
2024-05-19 08:55:59 +0000

    프론트엔드 translation table row edit mode 작동하도록 수정

--- src/frontend/src/pages/home/components/translations-table-row.tsx
+++ src/frontend/src/pages/home/components/translations-table-row.tsx
@@ -1,6 +1,11 @@
-import { IconButton } from "~/core/button";
+import { useState } from "react";
+import { FaEdit, FaSave } from "react-icons/fa";
+import { Button, IconButton } from "~/core/button";
 import { GetTranslationsResDto } from "~/core/codegen";
+import { Input } from "~/core/input";
+import { Flex } from "~/core/layout";
 import { Td, Tr } from "~/core/table";
+import { Text } from "~/core/text";
 import { useLayoutContext } from "~/layout/context";
 
 type TranslationsTableRowProps = {
@@ -11,23 +16,88 @@ export const TranslationsTableRow = ({
   translation,
 }: TranslationsTableRowProps) => {
   const { locales } = useLayoutContext();
+  const [editMode, setEditMode] = useState(false);
+  const [translationForm, setTranslationForm] = useState({
+    key: translation.key,
+    values: translation.values,
+  });
 
   return (
     <Tr _hover={{ bg: "gray.50" }} key={translation.id}>
-      <Td>{translation.key}</Td>
+      <Td>
+        {editMode ? (
+          <Input
+            size="sm"
+            value={translationForm.key}
+            onChange={(e) =>
+              setTranslationForm((prev) => ({
+                ...prev,
+                key: e.target.value,
+              }))
+            }
+          />
+        ) : (
+          <Text>{translation.key}</Text>
+        )}
+      </Td>
       {locales.map((locale) => {
         const valueObj = translation.values.find(
           (val) => val.localeId === locale.id
         );
-        return <Td key={locale.id}>{valueObj ? valueObj.value : ""}</Td>;
+        return (
+          <Td key={locale.id}>
+            {editMode ? (
+              <Input
+                size="sm"
+                value={
+                  translationForm.values.find(
+                    (val) => val.localeId === locale.id
+                  )?.value
+                }
+                onChange={(e) => {
+                  const newValues = translationForm.values.map((val) => {
+                    if (val.localeId === locale.id) {
+                      return {
+                        ...val,
+                        value: e.target.value,
+                      };
+                    }
+                    return val;
+                  });
+                  setTranslationForm((prev) => ({
+                    ...prev,
+                    values: newValues,
+                  }));
+                }}
+              />
+            ) : (
+              <Text>{valueObj?.value}</Text>
+            )}
+          </Td>
+        );
       })}
       <Td>
-        <IconButton
-          _hover={{ bg: "gray.200" }}
-          aria-label="edit"
-          size="xs"
-          variant="ghost"
-        />
+        <Flex gap={2}>
+          <IconButton
+            _hover={{ bg: "gray.200" }}
+            aria-label="edit"
+            colorScheme="gray"
+            icon={editMode ? <FaSave /> : <FaEdit />}
+            onClick={() => setEditMode((prev) => !prev)}
+            size="sm"
+            variant="outline"
+          />
+          <Button
+            _hover={{ bg: "gray.200" }}
+            colorScheme="gray"
+            onClick={() => setEditMode(false)}
+            size="sm"
+            variant="outline"
+            visibility={editMode ? "visible" : "hidden"}
+          >
+            X
+          </Button>
+        </Flex>
       </Td>
     </Tr>
   );
