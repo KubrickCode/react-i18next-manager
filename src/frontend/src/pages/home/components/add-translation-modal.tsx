b8235964b9864120c1cb3791b4ed9e3262397def
KubrickCode <kubrickcode@gmail.com>
2024-05-19 06:51:42 +0000

    프론트엔드 번역 리소스 추가할 수 있는 기능 추가

---
+++ src/frontend/src/pages/home/components/add-translation-modal.tsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+
+import { Button } from "~/core/button";
+import { AddTranslationReqBodyDto } from "~/core/codegen";
+import { LABELS } from "~/core/constants";
+import { Input } from "~/core/input";
+import { VStack } from "~/core/layout";
+import {
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+  ModalProps,
+} from "~/core/modal";
+import { useMutation } from "~/core/react-query";
+import { Text } from "~/core/text";
+import { useLayoutContext } from "~/layout/context";
+
+type AddTranslationModalProps = ModalProps;
+
+export const AddTranslationModal = ({
+  isOpen,
+  onClose,
+}: AddTranslationModalProps) => {
+  const { locales, selectedGroup } = useLayoutContext();
+
+  const [key, setKey] = useState("");
+  const [values, setValues] = useState<{ localeId: string; value: string }[]>(
+    locales.map((locale) => ({ localeId: locale.id, value: "" }))
+  );
+
+  const { mutate: addTranslation } = useMutation<AddTranslationReqBodyDto>({
+    refetchQueryKeys: [[`getTranslations-${selectedGroup?.id}}`]],
+  });
+
+  const handleSubmit = () => {
+    addTranslation({
+      link: `/translations/${selectedGroup?.id}`,
+      method: "post",
+      body: {
+        key,
+        values,
+      },
+    });
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalHeader>
+        <Text>Add Translation</Text>
+      </ModalHeader>
+      <ModalBody>
+        <VStack alignItems="baseline" gap={5}>
+          <VStack alignItems="baseline">
+            <Text fontSize="xs" fontWeight="lighter">
+              Group
+            </Text>
+            <Text fontWeight="semibold">{selectedGroup?.label}</Text>
+          </VStack>
+          <VStack alignItems="baseline" width="full">
+            <Text fontSize="xs" fontWeight="lighter">
+              Key
+            </Text>
+            <Input onChange={(e) => setKey(e.target.value)} value={key} />
+          </VStack>
+          {locales.map((locale) => (
+            <VStack key={locale.id} alignItems="baseline" width="full">
+              <Text fontSize="xs" fontWeight="lighter">
+                {locale.label}
+              </Text>
+              <Input
+                onChange={(e) =>
+                  setValues((prev) =>
+                    prev.map((val) => {
+                      if (val.localeId === locale.id) {
+                        return { ...val, value: e.target.value };
+                      }
+                      return val;
+                    })
+                  )
+                }
+                value={
+                  values.find((val) => val.localeId === locale.id)?.value || ""
+                }
+              />
+            </VStack>
+          ))}
+        </VStack>
+      </ModalBody>
+      <ModalFooter onClose={onClose}>
+        <Button colorScheme="primary" onClick={handleSubmit}>
+          {LABELS.SAVE}
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
+};
--- src/frontend/src/pages/home/components/translations-table-toolbar.tsx
+++ src/frontend/src/pages/home/components/translations-table-toolbar.tsx
@@ -3,8 +3,11 @@ import { IoMdRefresh } from "react-icons/io";
 import { Button, IconButton } from "~/core/button";
 import { SearchInput } from "~/core/input";
 import { HStack } from "~/core/layout";
+import { ModalToggle } from "~/core/modal";
 import { useQueryClient } from "~/core/react-query";
 import { useLayoutContext } from "~/layout/context";
+
+import { AddTranslationModal } from "./add-translation-modal";
 
 export const TranslationsTableToolbar = () => {
   const { selectedGroup } = useLayoutContext();
@@ -26,7 +29,9 @@ export const TranslationsTableToolbar = () => {
         onClick={() => handleRefresh()}
         size="sm"
       />
-      <Button size="sm">Add Key</Button>
+      <ModalToggle modal={AddTranslationModal}>
+        <Button size="sm">Add Key</Button>
+      </ModalToggle>
       <SearchInput size="sm" width="auto" />
     </HStack>
   );
