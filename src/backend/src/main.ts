14efc2c4e9d7e918f0fa20b1f1f61872f82f0abc
KubrickCode <kubrickcode@gmail.com>
2024-05-23 11:52:36 +0000

    백엔드 로깅 개발 환경에서만 뜨도록 수정

--- src/backend/src/main.ts
+++ src/backend/src/main.ts
@@ -5,7 +5,10 @@ import * as express from 'express';
 import { join } from 'path';
 
 const bootstrap = async () => {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger:
+      process.env.NODE_ENV === 'development' ? ['log', 'error'] : ['error'],
+  });
 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
@@ -25,6 +28,8 @@ const bootstrap = async () => {
       }
     },
   );
+
+  console.log('I18n studio is running on port', process.env.PORT || 4321);
 
   await app.listen(Number(process.env.PORT || 4321));
 };
