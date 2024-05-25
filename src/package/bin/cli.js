fe5d37631c7d73005b0e51735035797daae39a68
KubrickCode <kubrickcode@gmail.com>
2024-05-25 14:09:40 +0000

    studio 명령어 입력 시 config 파일 선택적으로 생성 가능한 기능 추가

--- src/package/bin/cli.js
+++ src/package/bin/cli.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 const { program } = require("commander");
+const fs = require("fs");
+const path = require("path");
+const { execSync } = require("child_process");
 
 program.version("0.3.9");
 
@@ -8,7 +11,35 @@ program
   .command("studio")
   .description("Start the server")
   .action(() => {
-    require("../dist/main.js");
+    const configFilePath = path.join(process.cwd(), "i18n-config.json");
+    if (!fs.existsSync(configFilePath)) {
+      const readline = require("readline");
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+
+      rl.question(
+        "i18n-config.json file not found. Do you want to create it? (y/n): ",
+        (answer) => {
+          if (answer.toLowerCase() === "y") {
+            console.log("Creating i18n-config.json file...");
+            execSync("npx @kubrick/react-i18next-manager init", {
+              stdio: "inherit",
+            });
+            require("../dist/main.js");
+          } else {
+            console.log(
+              "Please create the i18n-config.json file and restart the server."
+            );
+            process.exit(1);
+          }
+          rl.close();
+        }
+      );
+    } else {
+      require("../dist/main.js");
+    }
   });
 
 program
