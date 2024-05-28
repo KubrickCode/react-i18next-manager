07cd2c8c4692f5927bf5fcacf5a3d3b58a25a0ae
KubrickCode <kubrickcode@gmail.com>
2024-05-28 23:15:17 +0000

    fix: translation module 내 group repo 추가

--- src/backend/src/translation/translation.module.ts
+++ src/backend/src/translation/translation.module.ts
@@ -3,10 +3,11 @@ import { TranslationService } from './translation.service';
 import { TranslationRepository } from './translation.repository';
 import { TranslationController } from './translation.controller';
 import { DBModule } from 'src/db/db.module';
+import { GroupRepository } from 'src/group/group.repository';
 
 @Module({
   imports: [DBModule],
   controllers: [TranslationController],
-  providers: [TranslationService, TranslationRepository],
+  providers: [TranslationService, TranslationRepository, GroupRepository],
 })
 export class TranslationModule {}
