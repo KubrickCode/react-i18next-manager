29355ef2d39b4b315b4f9fd75fee7972556bb099
KubrickCode <kubrickcode@gmail.com>
2024-06-01 11:34:06 +0000

    실제 test-db 데이터 사용 가능하도록 테스트 코드 수정

--- src/backend/src/locale/locale.service.spec.ts
+++ src/backend/src/locale/locale.service.spec.ts
@@ -2,45 +2,36 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { LocaleService } from './locale.service';
 import { LocaleRepository } from './locale.repository';
 import { DBService } from 'src/db/db.service';
-import { generateUUID } from 'src/common/utils';
+import * as fs from 'fs';
+import * as path from 'path';
 
-describe('LocaleService', () => {
+describe('LocaleService Integration', () => {
   let service: LocaleService;
-  let repository: LocaleRepository;
+  let dbService: DBService;
 
   beforeEach(async () => {
-    const mockDBService = {
-      get: jest.fn().mockResolvedValue({
-        get: jest.fn().mockReturnThis(),
-        sortBy: jest.fn().mockReturnThis(),
-        value: jest.fn(),
-      }),
-    };
-
     const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        LocaleService,
-        LocaleRepository,
-        {
-          provide: DBService,
-          useValue: mockDBService,
-        },
-      ],
+      providers: [LocaleService, LocaleRepository, DBService],
     }).compile();
 
     service = module.get<LocaleService>(LocaleService);
-    repository = module.get<LocaleRepository>(LocaleRepository);
+    dbService = module.get<DBService>(DBService);
+
+    const db = await dbService.get();
+    const testData = JSON.parse(
+      fs.readFileSync(
+        path.resolve(__dirname, '../../test/test-db.json'),
+        'utf-8',
+      ),
+    );
+    db.setState(testData).write();
   });
 
   it('모든 locales 반환 성공', async () => {
-    const locales = [
-      { id: generateUUID(), label: 'en', position: 0 },
-      { id: generateUUID(), label: 'ko', position: 1 },
-    ];
-
-    jest.spyOn(repository, 'findMany').mockResolvedValue(locales);
-
     const result = await service.getAll();
-    expect(result).toEqual({ locales });
+    expect(result.locales).toEqual([
+      { id: '9094373b-d01c-4359-bf18-3cc66a04505b', label: 'en', position: 0 },
+      { id: 'c50010a4-7c43-432d-89d2-2cac5e44eee2', label: 'ko', position: 1 },
+    ]);
   });
 });
