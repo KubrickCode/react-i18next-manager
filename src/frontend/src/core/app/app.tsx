fd837b4e772247d56aa5118fa4e29183e2b09557
KubrickCode <kubrickcode@gmail.com>
2024-06-16 03:46:40 +0000

    i18n provider 및 초기 세팅 완료

--- src/frontend/src/core/app/app.tsx
+++ src/frontend/src/core/app/app.tsx
@@ -8,19 +8,22 @@ import { ModalProvider } from "../modal";
 import { AppContextProvider } from "./context";
 import { Layout } from "../layout";
 import { Loader } from "../loader";
+import { I18nProvider } from "../i18n";
 
 export const App = () => (
   <ThemeProvider>
-    <Layout>
-      <ReactQueryProvider>
-        <Suspense fallback={<Loader.FullScreen />}>
-          <AppContextProvider>
-            <ModalProvider>
-              <Router />
-            </ModalProvider>
-          </AppContextProvider>
-        </Suspense>
-      </ReactQueryProvider>
-    </Layout>
+    <I18nProvider>
+      <Layout>
+        <ReactQueryProvider>
+          <Suspense fallback={<Loader.FullScreen />}>
+            <AppContextProvider>
+              <ModalProvider>
+                <Router />
+              </ModalProvider>
+            </AppContextProvider>
+          </Suspense>
+        </ReactQueryProvider>
+      </Layout>
+    </I18nProvider>
   </ThemeProvider>
 );
---
+++ src/frontend/src/core/i18n/context.tsx
@@ -0,0 +1,63 @@
+import i18n from "i18next";
+import { createContext, useCallback, useContext, useState } from "react";
+import { I18nextProvider, initReactI18next } from "react-i18next";
+
+import resources from "./i18n.json";
+
+const DEFAULT_LANGUAGE = "en";
+
+const STORAGE_KEY = "i18n.lng";
+
+i18n.use(initReactI18next).init({
+  fallbackLng: DEFAULT_LANGUAGE,
+  interpolation: {
+    escapeValue: false,
+  },
+  keySeparator: false,
+  lng: localStorage.getItem(STORAGE_KEY) || DEFAULT_LANGUAGE,
+  resources,
+});
+
+type I18nContextType = {
+  changeLanguage: (language: string) => void;
+  language: string;
+};
+
+const I18nContext = createContext<I18nContextType | undefined>(undefined);
+
+export const I18nProvider = ({ children }: { children?: JSX.Element }) => {
+  const forceUpdate = useForceUpdate();
+
+  const changeLanguage = useCallback(
+    (language: string) => {
+      localStorage.setItem(STORAGE_KEY, language);
+      i18n.changeLanguage(language);
+
+      document.documentElement.setAttribute("lang", language);
+
+      forceUpdate();
+    },
+    [forceUpdate]
+  );
+
+  const context = {
+    changeLanguage,
+    language: i18n.language,
+  };
+
+  return (
+    <I18nContext.Provider value={context}>
+      <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
+    </I18nContext.Provider>
+  );
+};
+
+export const useI18n = () => useContext(I18nContext);
+
+const useForceUpdate = () => {
+  const [, setTick] = useState(0);
+
+  return useCallback(() => {
+    setTick((tick) => tick + 1);
+  }, []);
+};
