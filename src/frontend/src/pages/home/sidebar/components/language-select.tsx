bb3490e8a2098387c1195902a87c77b6f382db67
KubrickCode <kubrickcode@gmail.com>
2024-06-17 10:13:21 +0000

    Language Select를 통해 실제 브라우저가 언어 변경을 컨트롤할 수 있도록 수정

--- src/frontend/src/pages/home/sidebar/components/language-select.tsx
+++ src/frontend/src/pages/home/sidebar/components/language-select.tsx
@@ -1,20 +1,39 @@
 import { chakra } from "@chakra-ui/react";
+import _ from "lodash";
 
+import { useI18n } from "~/core/i18n";
 import { Select, SelectButton, SelectList } from "~/core/select";
 
-export const LanguageSelect = () => (
-  <Select
-    defaultValue="English"
-    name="language"
-    options={["English", "Korean"].map((lang) => ({
-      label: lang,
-      value: lang,
-    }))}
-  >
-    <CustomSelectButton width="100%" />
-    <SelectList />
-  </Select>
-);
+const supportedLanguages = [
+  { key: "en", label: "English" },
+  { key: "ko", label: "한국어" },
+];
+
+export const LanguageSelect = () => {
+  const { changeLanguage, language: currentLanguage } = useI18n();
+
+  const currentLanguageLabel = _.find(
+    supportedLanguages,
+    (language) => language.key === currentLanguage
+  )?.label;
+
+  return (
+    <Select
+      defaultValue={currentLanguageLabel}
+      name="language"
+      onChange={(value) => {
+        changeLanguage(value);
+      }}
+      options={supportedLanguages.map(({ key, label }) => ({
+        label,
+        value: key,
+      }))}
+    >
+      <CustomSelectButton width="100%" />
+      <SelectList />
+    </Select>
+  );
+};
 
 const CustomSelectButton = chakra(SelectButton, {
   baseStyle: {
