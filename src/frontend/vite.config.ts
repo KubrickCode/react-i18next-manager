1a80f129fa6277e5294dd6686ce63f067054a127
KubrickCode <kubrickcode@gmail.com>
2024-06-22 03:58:43 +0000

    vite 개발 환경에 따라 파비콘 다르게 보이도록 설정

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,18 +1,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), viteTsconfigPaths()],
-  server: {
-    open: true,
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3001",
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      react(),
+      viteTsconfigPaths(),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            VITE_FAVICON:
+              mode === "development" ? "logo-light.png" : "logo-dark.png",
+          },
+        },
+      }),
+    ],
+    server: {
+      open: true,
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: "http://localhost:3001",
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
