cc6f901160537488051218d4aa3d0424f6805fc9
KubrickCode <kubrickcode@gmail.com>
2024-07-01 23:00:49 +0000

    form footer 추가

---
+++ src/frontend/src/core/form/form-footer.tsx
@@ -0,0 +1,26 @@
+import { HTMLAttributes } from "react";
+import { useFormContext } from "react-hook-form";
+
+import { Box } from "../layout";
+import { Text } from "../text";
+
+export type FormFooterProps = HTMLAttributes<HTMLDivElement>;
+
+export const FormFooter = ({ children, ...otherProps }: FormFooterProps) => {
+  const {
+    formState: { errors },
+  } = useFormContext();
+
+  const formError = errors[""];
+
+  return (
+    <Box paddingTop={4} {...otherProps}>
+      {children}
+      {formError && formError.message && (
+        <Text color="danger" fontSize="sm" marginTop={1}>
+          {formError.message.toString()}
+        </Text>
+      )}
+    </Box>
+  );
+};
