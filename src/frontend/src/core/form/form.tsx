f4512089d7c040bb3c80bd9ad95aa3ba9e973b27
KubrickCode <kubrickcode@gmail.com>
2024-07-06 08:45:33 +0000

    Form 컴포넌트 내 children 사용가능하도록 수정

--- src/frontend/src/core/form/form.tsx
+++ src/frontend/src/core/form/form.tsx
@@ -1,9 +1,11 @@
+import { PropsWithChildren } from "react";
 import { FieldValues, UseFormReturn } from "react-hook-form";
 
 export type FormProps<FormValues extends FieldValues = FieldValues> = {
   onSubmit: ReturnType<UseFormReturn<FormValues>["handleSubmit"]>;
-};
+} & PropsWithChildren;
 
 export const Form = <FormValues extends FieldValues>({
+  children,
   onSubmit,
-}: FormProps<FormValues>) => <form onSubmit={onSubmit} />;
+}: FormProps<FormValues>) => <form onSubmit={onSubmit}>{children}</form>;
