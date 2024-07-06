236d3b45db7b63ba908feecfed295796060aaae7
KubrickCode <kubrickcode@gmail.com>
2024-07-06 04:23:30 +0000

    form 컴포넌트 Flex 기반에서 일반 form으로 변경

--- src/frontend/src/core/form/form.tsx
+++ src/frontend/src/core/form/form.tsx
@@ -1,20 +1,9 @@
 import { FieldValues, UseFormReturn } from "react-hook-form";
-import { Flex, FlexProps } from "../layout";
 
-export type FormProps<FormValues extends FieldValues = FieldValues> =
-  FlexProps & {
-    onSubmit: ReturnType<UseFormReturn<FormValues>["handleSubmit"]>;
-  };
+export type FormProps<FormValues extends FieldValues = FieldValues> = {
+  onSubmit: ReturnType<UseFormReturn<FormValues>["handleSubmit"]>;
+};
 
 export const Form = <FormValues extends FieldValues>({
   onSubmit,
-  ...otherProps
-}: FormProps<FormValues>) => (
-  <Flex
-    as="form"
-    direction="column"
-    gap={4}
-    onSubmit={onSubmit}
-    {...otherProps}
-  />
-);
+}: FormProps<FormValues>) => <form onSubmit={onSubmit} />;
