12aa667a1997800abcdb8a1f8ea2efd8498ec7cf
KubrickCode <kubrickcode@gmail.com>
2024-07-06 12:00:56 +0000

    그룹 이동 모달 MutationForm 적용

--- src/frontend/src/pages/home/main/components/move-translations-group-modal.tsx
+++ src/frontend/src/pages/home/main/components/move-translations-group-modal.tsx
@@ -1,13 +1,17 @@
-import { useState } from "react";
-
-import { Button } from "~/core/button";
 import {
   EditTranslationsParentGroupReqBodyDto,
   GetGroupsResDto,
 } from "~/core/codegen";
-import { z } from "~/core/form";
+import {
+  Field,
+  FieldError,
+  Fields,
+  Label,
+  MutationForm,
+  SubmitButton,
+  z,
+} from "~/core/form";
 import { i18nKeys, useTranslation } from "~/core/i18n";
-import { VStack } from "~/core/layout";
 import {
   Modal,
   ModalBody,
@@ -15,30 +19,14 @@ import {
   ModalHeader,
   ModalProps,
 } from "~/core/modal";
-import {
-  KEY,
-  ENDPOINT,
-  useMutation,
-  useSuspenseQuery,
-} from "~/core/react-query";
+import { KEY, ENDPOINT, useSuspenseQuery } from "~/core/react-query";
 import { Text } from "~/core/text";
 import { GroupTreeView } from "~/shared/group";
-
-const schema = z.object({
-  translations: z.array(
-    z.object({
-      id: z.string().uuid(),
-    })
-  ),
-  newGroupId: z.string().uuid(),
-});
 
 type Group = {
   id: string;
   label: string;
 };
-
-type SelectedGroup = Group | null;
 
 type MoveTranslationsGroupModalProps = ModalProps & {
   currentGroup: Group;
@@ -58,76 +46,69 @@ export const MoveTranslationsGroupModal = ({
     ENDPOINT.GET_GROUPS,
     KEY.GET_GROUPS_IN_MOVE_GROUP_MODAL
   );
-  const [selectedGroup, setSelectedGroup] = useState<SelectedGroup>(null);
-  const [error, setError] = useState<string | null>(null);
 
   const { groups } = data;
 
-  const { mutate: editTranslationsParentGroup } =
-    useMutation<EditTranslationsParentGroupReqBodyDto>({
-      refetchQueryKeys: [[KEY.GET_TRANSLATIONS(currentGroup.id)]],
-      schema,
-      toast: t(i18nKeys.group.moveGroupSuccess),
-    });
-
-  const handleSubmit = () => {
-    if (selectedGroup === null) {
-      setError(t(i18nKeys.group.selectTargetGroupEmptyError));
-      return;
-    }
-
-    editTranslationsParentGroup({
-      endpoint: ENDPOINT.EDIT_TRANSLATIONS_PARENT_GROUP,
-      method: "patch",
-      body: {
-        translations: translationIds.map((id) => ({ id })),
-        newGroupId: selectedGroup.id,
-      },
-    });
+  const handleComplete = () => {
     onClose();
     onComplete();
   };
 
-  const handleSelectedGroup = (group: SelectedGroup) => {
-    setSelectedGroup(group);
-  };
+  const schema = z.object({
+    translations: z.array(
+      z.object({
+        id: z.string().uuid(),
+      })
+    ),
+    newGroupId: z
+      .string({ message: t(i18nKeys.group.selectTargetGroupEmptyError) })
+      .uuid(),
+  });
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <ModalHeader>
-        <Text>{t(i18nKeys.group.moveGroup)}</Text>
-      </ModalHeader>
-      <ModalBody>
-        <VStack alignItems="baseline" gap={5}>
-          <VStack alignItems="baseline">
-            <Text fontSize="xs" fontWeight="lighter">
-              {t(i18nKeys.group.currentGroup)}
-            </Text>
-            <Text fontWeight="semibold">{currentGroup.label}</Text>
-          </VStack>
-          <VStack alignItems="baseline">
-            <Text fontSize="xs" fontWeight="lighter">
-              {t(i18nKeys.group.selectTargetGroup)}
-            </Text>
-            <GroupTreeView
-              groups={groups}
-              handleSelectedGroup={handleSelectedGroup}
-              height={300}
-              width={400}
-            />
-          </VStack>
-          {error && (
-            <Text color="danger" fontSize="sm">
-              {error}
-            </Text>
-          )}
-        </VStack>
-      </ModalBody>
-      <ModalFooter onClose={onClose}>
-        <Button colorScheme="darkgray" onClick={handleSubmit}>
-          {t(i18nKeys.common.save)}
-        </Button>
-      </ModalFooter>
+      <MutationForm<EditTranslationsParentGroupReqBodyDto>
+        defaultValues={{
+          translations: translationIds.map((id) => ({ id })),
+        }}
+        endpoint={ENDPOINT.EDIT_TRANSLATIONS_PARENT_GROUP}
+        method="patch"
+        onComplete={handleComplete}
+        refetchQueryKeys={[[KEY.GET_TRANSLATIONS(currentGroup.id)]]}
+        schema={schema}
+        toast={t(i18nKeys.group.moveGroupSuccess)}
+      >
+        {({ setValue }) => (
+          <>
+            <ModalHeader>
+              <Text>{t(i18nKeys.group.moveGroup)}</Text>
+            </ModalHeader>
+            <ModalBody>
+              <Fields>
+                <Field>
+                  <Label>{t(i18nKeys.group.currentGroup)}</Label>
+                  <Text fontWeight="bold">{currentGroup.label}</Text>
+                </Field>
+                <Field>
+                  <Label>{t(i18nKeys.group.selectTargetGroup)}</Label>
+                  <GroupTreeView
+                    groups={groups}
+                    handleSelectedGroup={(group) => {
+                      if (group) setValue("newGroupId", group.id);
+                    }}
+                    height={300}
+                    width={400}
+                  />
+                  <FieldError name="newGroupId" />
+                </Field>
+              </Fields>
+            </ModalBody>
+            <ModalFooter onClose={onClose}>
+              <SubmitButton />
+            </ModalFooter>
+          </>
+        )}
+      </MutationForm>
     </Modal>
   );
 };
