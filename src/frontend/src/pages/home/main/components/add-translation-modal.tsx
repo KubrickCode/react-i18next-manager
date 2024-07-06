4d2c915b5e464c16a48b6a53f23640054395b19c
KubrickCode <kubrickcode@gmail.com>
2024-07-06 09:59:50 +0000

    번역 추가 모달 내 mutation form 적용

--- src/frontend/src/pages/home/main/components/add-translation-modal.tsx
+++ src/frontend/src/pages/home/main/components/add-translation-modal.tsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
-
 import { useApp } from "~/core/app";
-import { Button } from "~/core/button";
 import { AddTranslationReqBodyDto } from "~/core/codegen";
-import { z } from "~/core/form";
+import {
+  Input,
+  Field,
+  Fields,
+  MutationForm,
+  SubmitButton,
+  z,
+} from "~/core/form";
 import { i18nKeys, useTranslation } from "~/core/i18n";
-import { Input } from "~/core/input";
-import { VStack } from "~/core/layout";
 import {
   Modal,
   ModalBody,
@@ -14,7 +16,7 @@ import {
   ModalHeader,
   ModalProps,
 } from "~/core/modal";
-import { KEY, ENDPOINT, useMutation } from "~/core/react-query";
+import { KEY, ENDPOINT } from "~/core/react-query";
 import { Text } from "~/core/text";
 import { replaceBlank } from "~/core/utils";
 
@@ -43,82 +45,63 @@ export const AddTranslationModal = ({
   const { t } = useTranslation();
   const { locales } = useApp();
 
-  const [key, setKey] = useState("");
-  const [values, setValues] = useState<{ localeId: string; value: string }[]>(
-    locales.map((locale) => ({ localeId: locale.id, value: "" }))
-  );
-
-  const { mutate: addTranslation } = useMutation<AddTranslationReqBodyDto>({
-    refetchQueryKeys: [[KEY.GET_TRANSLATIONS(selectedGroup.id)]],
-    schema,
-    toastMessage: t(i18nKeys.translation.addTranslationSuccess),
-  });
-
-  const handleSubmit = () => {
-    addTranslation({
-      endpoint: ENDPOINT.ADD_TRANSLATION(selectedGroup.id),
-      method: "post",
-      body: {
-        key,
-        values,
-      },
-    });
-    onClose();
-  };
-
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <MutationForm>
-        <ModalHeader>
-          <Text>{t(i18nKeys.translation.addTranslation)}</Text>
-        </ModalHeader>
-        <ModalBody>
-          <VStack alignItems="baseline" gap={5}>
-            <VStack alignItems="baseline">
-              <Text fontSize="xs" fontWeight="lighter">
-                {t(i18nKeys.common.group)}
-              </Text>
-              <Text fontWeight="semibold">{selectedGroup.label}</Text>
-            </VStack>
-            <VStack alignItems="baseline" width="full">
-              <Text fontSize="xs" fontWeight="lighter">
-                {t(i18nKeys.common.key)}
-              </Text>
-              <Input
-                onChange={(e) => setKey(replaceBlank(e.target.value))}
-                value={key}
-              />
-            </VStack>
-            {locales.map((locale) => (
-              <VStack key={locale.id} alignItems="baseline" width="full">
-                <Text fontSize="xs" fontWeight="lighter">
-                  {locale.label}
-                </Text>
-                <Input
-                  onChange={(e) =>
-                    setValues((prev) =>
-                      prev.map((val) => {
-                        if (val.localeId === locale.id) {
-                          return { ...val, value: e.target.value };
-                        }
-                        return val;
-                      })
-                    )
-                  }
-                  value={
-                    values.find((val) => val.localeId === locale.id)?.value ||
-                    ""
-                  }
-                />
-              </VStack>
-            ))}
-          </VStack>
-        </ModalBody>
-        <ModalFooter onClose={onClose}>
-          <Button colorScheme="darkgray" onClick={handleSubmit}>
-            {t(i18nKeys.common.save)}
-          </Button>
-        </ModalFooter>
+      <MutationForm<AddTranslationReqBodyDto>
+        endpoint={ENDPOINT.ADD_TRANSLATION(selectedGroup.id)}
+        method="post"
+        onComplete={onClose}
+        refetchQueryKeys={[[KEY.GET_TRANSLATIONS(selectedGroup.id)]]}
+        schema={schema}
+        toastMessage={t(i18nKeys.translation.addTranslationSuccess)}
+      >
+        {({ setValue }) => (
+          <>
+            <ModalHeader>
+              <Text>{t(i18nKeys.translation.addTranslation)}</Text>
+            </ModalHeader>
+            <ModalBody>
+              <Fields>
+                <Field>
+                  <Text fontSize="xs" fontWeight="lighter">
+                    {t(i18nKeys.common.group)}
+                  </Text>
+                  <Text fontWeight="semibold">{selectedGroup.label}</Text>
+                </Field>
+                <Field>
+                  <Text fontSize="xs" fontWeight="lighter">
+                    {t(i18nKeys.common.key)}
+                  </Text>
+                  <Input
+                    name="key"
+                    onChange={(e) =>
+                      setValue("key", replaceBlank(e.target.value))
+                    }
+                  />
+                </Field>
+                {locales.map((locale, index) => (
+                  <Field key={locale.id}>
+                    <Text fontSize="xs" fontWeight="lighter">
+                      {locale.label}
+                    </Text>
+                    <Input
+                      name={`values.${index}.value`}
+                      onChange={(e) =>
+                        setValue(`values.${index}`, {
+                          localeId: locale.id,
+                          value: e.target.value,
+                        })
+                      }
+                    />
+                  </Field>
+                ))}
+              </Fields>
+            </ModalBody>
+            <ModalFooter onClose={onClose}>
+              <SubmitButton />
+            </ModalFooter>
+          </>
+        )}
       </MutationForm>
     </Modal>
   );
