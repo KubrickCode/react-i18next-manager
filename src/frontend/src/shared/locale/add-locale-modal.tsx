4b6f1aed7e5bccc1251f43802f6dc6e3bace5fb9
KubrickCode <kubrickcode@gmail.com>
2024-07-06 12:30:47 +0000

    언어 코드 추가 모달에 MutationForm 적용

--- src/frontend/src/shared/locale/add-locale-modal.tsx
+++ src/frontend/src/shared/locale/add-locale-modal.tsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
-
-import { Button } from "~/core/button";
 import { AddLocaleReqBodyDto } from "~/core/codegen";
-import { z } from "~/core/form";
+import {
+  Field,
+  Label,
+  MutationForm,
+  z,
+  Input,
+  SubmitButton,
+} from "~/core/form";
 import { i18nKeys, useTranslation } from "~/core/i18n";
-import { Input } from "~/core/input";
-import { VStack } from "~/core/layout";
 import {
   Modal,
   ModalBody,
@@ -13,7 +15,7 @@ import {
   ModalHeader,
   ModalProps,
 } from "~/core/modal";
-import { KEY, ENDPOINT, useMutation } from "~/core/react-query";
+import { KEY, ENDPOINT } from "~/core/react-query";
 import { Text } from "~/core/text";
 
 const schema = z.object({
@@ -22,56 +24,40 @@ const schema = z.object({
 });
 
 type AddLocaleModalProps = ModalProps & {
-  onComplete?: () => void;
   position: number;
 };
 
 export const AddLocaleModal = ({
   isOpen,
   onClose,
-  onComplete,
   position,
 }: AddLocaleModalProps) => {
   const { t } = useTranslation();
-  const [label, setLabel] = useState("");
-
-  const { mutate: addLocale } = useMutation<AddLocaleReqBodyDto>({
-    refetchQueryKeys: [[KEY.GET_LOCALES]],
-    schema,
-    toast: t(i18nKeys.setting.addLocaleSuccess),
-  });
-
-  const handleSubmit = () => {
-    addLocale({
-      endpoint: ENDPOINT.ADD_LOCALE,
-      method: "post",
-      body: {
-        label,
-        position,
-      },
-    });
-    onClose();
-    onComplete?.();
-  };
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <ModalHeader>
-        <Text>{t(i18nKeys.setting.addLocale)}</Text>
-      </ModalHeader>
-      <ModalBody>
-        <VStack alignItems="baseline" width="full">
-          <Text fontSize="xs" fontWeight="lighter">
-            {t(i18nKeys.setting.newLocale)}
-          </Text>
-          <Input onChange={(e) => setLabel(e.target.value)} value={label} />
-        </VStack>
-      </ModalBody>
-      <ModalFooter onClose={onClose}>
-        <Button colorScheme="darkgray" onClick={handleSubmit}>
-          {t(i18nKeys.common.save)}
-        </Button>
-      </ModalFooter>
+      <MutationForm<AddLocaleReqBodyDto>
+        defaultValues={{ position }}
+        endpoint={ENDPOINT.ADD_LOCALE}
+        method="post"
+        onComplete={onClose}
+        refetchQueryKeys={[[KEY.GET_LOCALES]]}
+        schema={schema}
+        toast={t(i18nKeys.setting.addLocaleSuccess)}
+      >
+        <ModalHeader>
+          <Text>{t(i18nKeys.setting.addLocale)}</Text>
+        </ModalHeader>
+        <ModalBody>
+          <Field>
+            <Label>{t(i18nKeys.setting.newLocale)}</Label>
+            <Input name="label" />
+          </Field>
+        </ModalBody>
+        <ModalFooter onClose={onClose}>
+          <SubmitButton />
+        </ModalFooter>
+      </MutationForm>
     </Modal>
   );
 };
