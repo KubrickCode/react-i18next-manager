82bc31a1806d545ebbc62df7800d0b20f6f4b0a9
KubrickCode <kubrickcode@gmail.com>
2024-07-07 11:12:15 +0000

    프론트엔드 그룹 이동 기능 부모 그룹 또한 변경 가능하도록 수정

--- src/frontend/src/shared/group/treeview/group-treeview.tsx
+++ src/frontend/src/shared/group/treeview/group-treeview.tsx
@@ -18,6 +18,7 @@ import { AddGroupModal } from "./add-group-modal";
 import { GroupTreeviewNode } from "./group-treeview-node";
 
 const schema = z.object({
+  parentId: z.string().nullable(),
   position: z.number().int(),
 });
 
@@ -100,11 +101,11 @@ export const GroupTreeView = ({
         indent={24}
         onMove={
           needsMutation
-            ? ({ dragIds, index: position }) => {
+            ? ({ dragIds, index: position, parentId }) => {
                 editGroupPosition({
                   endpoint: ENDPOINT.EDIT_GROUP_POSITION(dragIds[0]),
                   method: "patch",
-                  body: { position },
+                  body: { parentId, position },
                 });
               }
             : undefined
