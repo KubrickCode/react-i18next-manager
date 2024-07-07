6fb5b98071985242f00c9981c45d0057c1b81f9c
KubrickCode <kubrickcode@gmail.com>
2024-07-07 09:07:46 +0000

    zod custom error 메시지 추가

--- src/frontend/src/core/form/zod.tsx
+++ src/frontend/src/core/form/zod.tsx
@@ -1,3 +1,19 @@
 import { z as _z } from "zod";
 
+const customErrorMap: _z.ZodErrorMap = (issue, ctx) => {
+  console.error("form validation issue: ", issue);
+
+  if (
+    issue.code === z.ZodIssueCode.invalid_type &&
+    issue.expected === "string" &&
+    (issue.received === "undefined" || issue.received === "null")
+  ) {
+    return { message: `${issue.path[0]} is required` };
+  }
+
+  return { message: issue.message ?? ctx.defaultError };
+};
+
+_z.setErrorMap(customErrorMap);
+
 export const z = _z;
