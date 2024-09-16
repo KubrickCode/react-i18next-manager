4204c53fff55dbbb4e87ef2a18647947fad5938e
KubrickCode <kubrickcode@gmail.com>
2024-09-16 13:48:57 +0000

    Vite 서버 설정에 host: true 추가
    - Docker 업데이트 이후 네트워크 설정이 변경된 것으로 유추되어 vite 서버에서 localhost 접근이 불가능해짐.
    - 임시 해결책으로 Vite 설정 파일에 host: true 옵션을 추가하여 모든 네트워크 인터페이스에서 접근 가능하도록 설정함.
    - 이 설정은 보안상 권장되지 않으므로, 원인을 파악하고 근본적인 해결책을 찾는 것이 필요하고, TODO 주석을 통해 지속적인 모니터링 필요성을 명시함.

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -24,6 +24,12 @@ export default defineConfig(({ mode }) => {
       }),
     ],
     server: {
+      /**
+       * TODO: Docker 업데이트 이후 네트워크 설정이 변경된 것으로 유추되어 vite 서버에서 `localhost` 접근이 불가능해짐.
+       * 임시 해결책으로 `host: true` 설정을 추가하여 모든 네트워크 인터페이스에서 접근 가능하도록 설정함.
+       * 이 설정은 보안상 권장되지 않으며, 원인을 파악하고 근본적인 해결책을 찾는 것이 필요함. 지속적인 모니터링 필요.
+       */
+      host: true,
       open: true,
       port: 3000,
       proxy: {
