6ba53692bea5c5c0825e7e09f3abaaf449d037f3
KubrickCode <kubrickcode@gmail.com>
2024-11-10 11:19:32 +0000

    불필요한 라우터 Path 상수 제거

--- src/frontend/src/core/router/router.tsx
+++ src/frontend/src/core/router/router.tsx
@@ -2,16 +2,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import { HomePage, NotFoundPage } from "~/pages";
 
-const PATH = {
-  HOME: "/",
-  NOT_FOUND: "*",
-};
-
 export const Router = () => (
   <BrowserRouter>
     <Routes>
-      <Route path={PATH.HOME} element={<HomePage />} />
-      <Route path={PATH.NOT_FOUND} element={<NotFoundPage />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   </BrowserRouter>
 );
