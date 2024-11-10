d38f5f35c6772885cc595016733ec61f5209a8e1
KubrickCode <kubrickcode@gmail.com>
2024-11-10 11:25:02 +0000

    홈페이지 라우터 폴더 index 경로 변경

--- src/frontend/src/core/router/router.tsx
+++ src/frontend/src/core/router/router.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import { HomePage } from "~/pages";
+import { HomePage } from "~/pages/home";
 
 export const Router = () => (
   <BrowserRouter>
